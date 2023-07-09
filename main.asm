dc68bbd07d0d1a8cead1834ab5c23b8e57222030
Ilya Akkuzin <gr3yknigh1@gmail.com>
2023-07-09 11:48:15 +0300

    Reading input from user!

--- main.asm
+++ main.asm
@@ -8,12 +8,30 @@ global _start
 %define EXIT_SUCCESS	0
 %define EXIT_FAILURE	1
 
+
+%define SYSCALL_READ	0
 %define SYSCALL_WRITE	1
 %define SYSCALL_EXIT	60
+
+
+%define BUF_SIZE	64
 
 segment .text
 _start:
 	mov	rax, SYSCALL_WRITE
+	mov	rsi, S0
+	mov	rdx, S0_L
+	mov	rdi, UNIX_FD_STDOUT
+	syscall
+
+	mov	rax, UNIX_FD_STDIN
+	mov	rsi, B0
+	mov	rdx, BUF_SIZE
+	syscall
+
+	mov	rax, SYSCALL_WRITE
+	mov	rsi, B0
+	mov	rdx, BUF_SIZE
 	mov	rdi, UNIX_FD_STDOUT
 	mov	rsi, msg
 	mov	rdx, ln
@@ -26,5 +44,8 @@ _start:
 	syscall
 
 segment .data
-	msg	db	'word', 10, 0
-	ln	equ	$-msg
+	S0	db	"Enter your name, please: ", 0
+	S0_L	equ	$-S0
+
+segment .bss
+	B0	resb	BUF_SIZE
